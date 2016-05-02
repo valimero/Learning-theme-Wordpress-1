8047411331ac30ecb2ed40a3f614c247b854cc3c
Valentin Coppin <valimero.ilou@gmail.com>
2016-05-02 14:56:11 +0200

    index > article > add margin

---
+++ single.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<?php if(have_posts()): ?>
+	<?php while(have_posts()) : the_post(); ?>
+		<div class="article">
+			<p class="titre_article"><?php the_title(); ?></p>
+			<p class="content"><?php the_content(); ?></p>
+		</div>
+	<?php endwhile; ?>
+<?php endif; ?>
+
+
+<?php get_footer(); ?>
