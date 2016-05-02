e9c45c845a428c12868b811132f052b6ec6b8467
Valentin Coppin <valimero.ilou@gmail.com>
2016-05-02 14:51:31 +0200

    add content

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <?php if(have_posts()): ?>
 	<?php while(have_posts()) : the_post(); ?>
 		<div class="article">
-			<?php the_title(); ?>
+			<p class="titre_article"><?php the_title(); ?></p>
+			<p class="content"><?php the_content(); ?></p>
 		</div>
 	<?php endwhile; ?>
 <?php endif; ?>
