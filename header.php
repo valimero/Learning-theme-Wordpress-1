fecee79c71f9b09f1e742db18f04b3ae49a4e3a8
Valentin Coppin <valimero.ilou@gmail.com>
2016-05-02 12:21:37 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title><?php bloginfo('name'); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen"></head>
+<body>
+
+	<div id="header">
+		<h1 class="titredusite">
+			<a href="<?php bloginfo('url');?>">
+				<?php bloginfo('name'); ?></a>
+		</h1>
+	</div>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+Contenu
+<?php get_footer(); ?>
